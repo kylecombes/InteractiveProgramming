5bb9ae39a22d002956068e27dd0485c7b570cc94
Kyle Combes <kylecombes@gmail.com>
2017-02-26 10:18:59 -0500

    Some refactoring

--- IceCreamGame.py
+++ IceCreamGame.py
@@ -3,10 +3,8 @@
 
 import pygame
 import time
-import random
-from helpers import *
-from pygame.locals import *
-from ice_cream_cone import IceCreamCone
+# from helpers import *
+from graphics.ice_cream_cone import IceCreamCone
 
 pygame.init()
 
@@ -14,50 +12,54 @@ pygame.init()
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
-green = (255,0,0)
-blue = (255,0,0)
-# a fourth parameter is degree of tranparency
+green = (0,255,0)
+blue = (0,0,255)
+# a fourth parameter is degree of transparency
 
 font = pygame.font.SysFont(None,50)
 
-display_height = 600 #size of screen
-display_width = 700 #size of screen
+display_height = 600  # size of screen
+display_width = 700  # size of screen
 
-FPS = 10 #frames per second
+FPS = 10  # frames per second
 
 #surface that will be updated
-gameDisplay = pygame.display.set_mode((display_width, display_height))  #tuple of scsreen size
+screen = pygame.display.set_mode((display_width, display_height))  # tuple of screen size
 
 pygame.display.set_caption('The Best Ice Cream Game Known to Man')
 
-#pygame.display.update() #a parameter passes to this will update a specifici thing, but leaving it blank makes it update all
+#pygame.display.update() #a parameter passes to this will update a specified thing, but leaving it blank makes it update all
 #pygame.display.flip() is the same thing as pygame.display.update
 
+background = Background('space.png', [0,0])
+
+
+gameLoop()
+pygame.display.update()
+time.sleep(2)
+pygame.quit()  # uninitializes pygame
+quit()  # must have a quit
 
 
 class Background(pygame.sprite.Sprite):
+
 	def __init__(self, image_file, location):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load(image_file)
 		self.rect = self.image.get_rect()
 		self.rect.left, self.rect.top = location
 
-BackGround = Background('space.png',[0,0])
 
-def Cone():
-	img = pygame.image.load('assets/img/cone.png')
-	gameDisplay.blit(img, [200,400] )
+def game_loop():
 
-
-def gameLoop():
-
-	gameExit = False
-	clock = pygame.time.Clock() #setting up frames per second
+	game_exit = False
+	clock = pygame.time.Clock()  # setting up frames per second
+	cone = IceCreamCone(1, 100, 100)
 	#game loop
-	while not gameExit:
-		for event in pygame.event.get(): #getting the events
+	while not game_exit:
+		for event in pygame.event.get():  # getting the events
 			if event.type == pygame.QUIT:
-				gameExit = True
+				game_exit = True
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
 					pass
@@ -72,24 +74,16 @@ def gameLoop():
 
 		#making the background
 		#fill is something you apply to a surface object
-		gameDisplay.fill(white)#one way to draw
+		screen.fill(white)  # one way to draw
 
 
 		#between fill (above) and update is where you render other graphics
-		gameDisplay.blit(BackGround.image, BackGround.rect)
+		screen.blit(background.image, background.rect)
 
-		pygame.draw.rect(gameDisplay,white,[100, 100, 20,20])
+		pygame.draw.rect(screen, white, [100, 100, 20,20])
 
-		Cone()
+		cone.draw(screen)
 
-		pygame.display.update() #updates the screen (for every run through the loop)
+		pygame.display.update()  # updates the screen (for every run through the loop)
 
-		clock.tick(FPS)#setting frames per second
-
-
-
-gameLoop()
-pygame.display.update()
-time.sleep(2)
-pygame.quit() #uninitalizes pygame
-quit()#must have a quit
+		clock.tick(FPS)  # setting frames per second
